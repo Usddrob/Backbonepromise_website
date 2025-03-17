cb3a688c541ca92b6c15494e97f80167c8f5c1c8
Usddrob <usddrob@gmail.com>
2025-03-17 07:25:47 -0700

    Update

--- js/swiperItem.6db6e5a4.js
+++
@@ -1,2 +0,0 @@
-const e=new Swiper(".image-gallery__thumbs",{slidesPerView:"auto",freeMode:!0,watchSlidesProgress:!0});new Swiper(".image-gallery__main",{spaceBetween:10,zoom:!0,thumbs:{swiper:e}});
-//# sourceMappingURL=swiperItem.6db6e5a4.js.map
